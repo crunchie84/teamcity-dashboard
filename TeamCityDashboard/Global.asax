938e4ad60f7c10d02ab35096e5e1337e3d3a6ecb
Mark van Straten <mark@q42.nl>
2013-01-09 12:00:47 +0100

    hotwired log4net logging

--- TeamCityDashboard/Global.asax
+++ TeamCityDashboard/Global.asax
@@ -1,1 +1,1 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TeamCityDashboard.MvcApplication" Language="C#" %>
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TeamCityDashboard.TeamCityDashboardApplication" Language="C#" %>
--- TeamCityDashboard/Global.asax.cs
+++ TeamCityDashboard/Global.asax.cs
@@ -1,18 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Reflection;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
 using ITCloud.Web.Routing;
+using log4net;
 
 namespace TeamCityDashboard
 {
-  // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
-  // visit http://go.microsoft.com/?LinkId=9394801
-
-  public class MvcApplication : System.Web.HttpApplication
+  public class TeamCityDashboardApplication : System.Web.HttpApplication
   {
+    private static readonly ILog log = LogManager.GetLogger(typeof(TeamCityDashboardApplication));
+
     public static void RegisterGlobalFilters(GlobalFilterCollection filters)
     {
       filters.Add(new HandleErrorAttribute());
@@ -35,10 +37,45 @@ namespace TeamCityDashboard
 
     protected void Application_Start()
     {
+      log4net.Config.XmlConfigurator.Configure();
+
+      log.Info("Teamcity dashboard starting...");
+
+      string version = FileVersionInfo.GetVersionInfo(Assembly.GetExecutingAssembly().Location).ProductVersion;
+      if (string.IsNullOrWhiteSpace(version))
+        version = Assembly.GetExecutingAssembly().GetName().Version.ToString();
+      log.InfoFormat("Current version: '{0}'", version);
+
       AreaRegistration.RegisterAllAreas();
       
       RegisterGlobalFilters(GlobalFilters.Filters);
       RegisterRoutes(RouteTable.Routes);
+
+      log.Info("Application started");
+    }
+
+    protected void Application_Error(object sender, EventArgs e)
+    {
+      if (log.IsErrorEnabled)
+      {
+        HttpApplication application = (HttpApplication)sender;
+        if (application.Context == null)
+          return;
+
+        var exception = application.Context.Error;
+
+        string logMessage = "An uncaught exception occurred";
+        try
+        {
+          if (Request != null && Request.Url != null)
+            logMessage += string.Format(" in {0} {1} (referrer={2})", Request.HttpMethod, Request.Url.PathAndQuery, Request.UrlReferrer);
+        }
+        catch (HttpException)
+        {
+          // an HttpException could occur with message: "Request is not available in this context" but then we ignore it
+        }
+        log.Error(logMessage, exception);
+      }
     }
   }
 }
