76ab0e8320d0ce386b10956ee5a0d23216aa7706
Mark van Straten <mark@q42.nl>
2013-01-14 11:43:37 +0100

    figured out which properties to display

--- TeamCityDashboard/Controllers/PushEvent.cs
+++ TeamCityDashboard/Controllers/PushEvent.cs
@@ -7,5 +7,33 @@ namespace TeamCityDashboard.Models
 {
   public class PushEvent
   {
+    /// <summary>
+    /// event=>actor=>login
+    /// </summary>
+    public string ActorUsername { get; set; }
+    /// <summary>
+    /// event=>actor=>gravatar_id
+    /// </summary>    
+    public string ActorGravatarId { get; set; }
+
+    /// <summary>
+    /// event=>payload=>size
+    /// </summary>
+    public int AmountOfCommits { get; set; }
+
+    /// <summary>
+    /// event=>repo=>name
+    /// </summary>
+    public string RepositoryName { get; set; }
+    
+    /// <summary>
+    /// event=>payload=>ref
+    /// </summary>    
+    public string BranchName { get; set; }
+
+    /// <summary>
+    /// event=>created_at
+    /// </summary>
+    public DateTime Created { get; set; }
   }
 }
